5a336a1da40e0ac9e3b8079e33ef3a729807e063
Enoch <49845680+enoch-xxiv@users.noreply.github.com>
2024-07-03 11:23:33 +0100

    add basic table functionality

--- script.js
+++ script.js
@@ -1,17 +1,25 @@
-const myLibrary = [];
+const table = document.getElementById("table")
+const tableBody = document.querySelector("#table tbody");
 
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author; 
     this.pages = pages;
     this.read = read;
+};
+
+const library = [];
+
+function populateTable() {
+    library.forEach((book) => {
+        let tableRow = document.createElement("tr");
+        for (const property in book) {
+            let tableData = document.createElement("td");
+            tableData.textContent = property === "read" ? (book[property] ? "✅" : "❌") : book[property];
+            tableRow.appendChild(tableData);
+        }
+        tableBody.appendChild(tableRow);
+    });
 }
 
-Book.prototype.info = function() {
-    return `${this.title}, by ${this.author}, ${this.pages}, pages long, ${this.read ? `has been read` : `has not been read yet`}`
-} 
-
-
-function addToLibrary() {
-    // do stuff here
-}
+library.length > 0 ? populateTable() : table.style.display = "none"
